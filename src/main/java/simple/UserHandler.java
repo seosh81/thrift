779739ebc230c8f7ad142ee2d6926cdb581aa946
silver <seosh81@gmail.com>
2012-03-27 17:53:27 +0900

    base files

---
+++ src/main/java/simple/UserHandler.java
@@ -0,0 +1,47 @@
+package simple;
+
+import java.sql.SQLException;
+
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+@Component
+public class UserHandler implements UserService.Iface {
+
+	@Autowired SqlMapClient sqlMap;
+	
+	@Override
+	public void store(User user) throws TException {
+		
+		System.out.println("in store");
+		try {
+			sqlMap.update("userSvc.addUser", user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("out store");
+	}
+
+	@Override
+	public User get(String id) throws TException {
+		User user = null;
+		try {
+			
+			user = (User) sqlMap
+					.queryForObject("userSvc.getUser", id);
+			System.out.println("delete count " + sqlMap.update("userSvc.deleteUser", user.getId()));
+			Thread.sleep(10000);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(user);
+		return user;
+	}
+
+}
